378fdeaa35dd30991c1b5cc012c58a44953fc7a3
Bebe-dev <praiseisinkaye@gmail.com>
2023-09-08 09:44:17 +0100

    First push

---
+++ index.js
@@ -0,0 +1,27 @@
+var day = document.getElementById('day');
+var time = document.getElementById('time');
+var utcTime = document.getElementById('utc')
+const currentDate = new Date();
+const getDay = () => {
+  const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const dayNumber = currentDate.getDay()
+  day.innerHTML = `Day: ${week[dayNumber]}`
+}
+getDay()
+
+const getTime = () => {
+  var currentHour = currentDate.getHours() - 1;
+  if (currentHour<10){
+    currentHour = `0${currentHour}`
+  }
+  var currentMinutes = currentDate.getMinutes();
+  if (currentMinutes<10){
+    currentMinutes = `0${currentMinutes}`
+  }
+  time.innerHTML = `Time: ${currentHour}:${currentMinutes}`
+  
+  
+
+}
+getTime()
